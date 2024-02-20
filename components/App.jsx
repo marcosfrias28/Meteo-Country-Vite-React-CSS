0d5272d2bbe59a92172bf7226418db2ac1bc1ce8
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-02-20 01:55:01 +0100

    Placeholder to the input

--- components/App.jsx
+++ components/App.jsx
@@ -39,12 +39,12 @@ export default function App () {
       <div className='search-container'>
         <MeteoCountryLogo className='logo' />
         <div className='search-bar'>
-          <div className='search'>
+          <search className='search'>
             <label htmlFor='search'>
               <svg xmlns='http://www.w3.org/2000/svg' height='24' fill='#f7f7f7' viewBox='0 -960 960 960' width='24'><path d='M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z' /></svg>
             </label>
-            <input onChange={handleChangeSearch} type='text' id='search' />
-          </div>
+            <input onChange={handleChangeSearch} type='text' id='search' placeholder='Italy, Russia, United State, Austral...'/>
+          </search>
         </div>
       </div>
       <article className='text-search'>
